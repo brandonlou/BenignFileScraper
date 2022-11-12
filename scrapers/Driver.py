e2eb8ebcc1e4c8595e48463f4c93b370be7ec858
Brandon Lou <14984696+brandonlou@users.noreply.github.com>
2022-11-11 20:32:44 -0800

    Add more arguments to improve reliability

--- scrapers/Driver.py
+++ scrapers/Driver.py
@@ -7,17 +7,23 @@ class Driver:
 
     def __init__(self, download_dir, headless):
         options = Options()
-        options.add_argument('start-maximized') #doesn't seem to work
+        options.add_argument('--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"')
+        options.add_argument('start-maximized') # Doesn't seem to work
         options.add_argument('enable-automation')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-gpu')
         options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--disable-browser-side-navigation')
+        options.add_argument('--dns-prefetch-disable')
         if headless:
             options.add_argument('--headless')
-        options.add_extension('/Users/brandonlou/Downloads/ublock.crx')
+        options.add_extension('/Users/brandon/Downloads/ublock.crx')
         prefs = {'download.default_directory': download_dir}
         options.add_experimental_option('prefs', prefs)
+        options.add_experimental_option('excludeSwitches', ['enable-automation']) # Doesn't seem to work
+        options.add_experimental_option('useAutomationExtension', False) # Doesn't seem to work
         self.driver = webdriver.Chrome(chrome_options=options, executable_path=os.environ['CHROMEDRIVER_PATH'])
+        self.driver.implicitly_wait(10) # Seconds
 
 
     def get_driver(self):
